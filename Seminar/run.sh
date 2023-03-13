ccc2a684e3efdfc6206e7b350673c411e0f1f548
nivedr <nived.rajaraman@gmail.com>
2023-03-13 12:33:08 -0700

    add test speaker

--- Seminar/run.sh
+++ Seminar/run.sh
@@ -9,6 +9,6 @@ read msg
 
 git add .
 git commit -m "$msg"
-git push
+git push --set-upstream origin master
 
 rsync -av /Users/nived.rajaraman/Dropbox/websites/bliss-website-and-seminar/ -e ssh nived.rajaraman@login.eecs.berkeley.edu:/home/eecs/jiantao/public_html/bliss
